23f7b5985632a7518284719d8c3d9fd47ae1d346
Lateefah-UK <106474932+Lateefah-UK@users.noreply.github.com>
2022-11-15 22:52:42 +0100

    week 6and7 task on image slider added via upload

---
+++ Responsive-SlideShowithCarousel/script.js
@@ -0,0 +1,42 @@
+const 
+  myslider = document.querySelectorAll('.myslider'), 
+  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+  counter += 1;
+  slidefun(counter);
+}
+function plusSlide(n) {
+  counter += n;
+  slidefun(counter);
+  resetTimer();
+}
+function currentSlide(n) {
+  counter = n;
+  slidefun(counter);
+  resetTimer();
+}
+function resetTimer() {
+  clearInterval(timer);
+  timer = setInterval(autoSlide, 8000);
+}
+function slidefun(n) {
+  let i;
+  for(i=0;i<myslider.length;i++){
+    myslider[i].style.display="none";
+  }
+  for(i=0;i<dot.length;i++){
+    dot[i].className = dot[i].className.replace('active','');
+  }
+  if(n>myslider.length){
+    counter = 1;
+  }
+  if(n<1){
+    counter = myslider.length;
+  }
+
+  myslider[counter-1].style.display = "block";
+  dot[counter-1].className +="active";
+}
